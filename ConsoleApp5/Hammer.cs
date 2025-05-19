fbcc8b58bfa3fbc6230a80f52ca105b6a9c302a0
Inon Guetta <inon16guetta@gmail.com>
2025-05-19 19:59:55 +0300

    third update sulotion to 1/2/3

---
+++ ConsoleApp5/Hammer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Hammer : Tool
+{
+
+
+
+    
+    // המתודות שעשינו להם פולימורפיזם והשתמשנו אצלם ב override
+    // קונסטרקטור
+    public Hammer(string name, double weight) : base(name, weight)
+    {
+
+
+    }
+
+
+
+    public override void Describe()
+    {
+        Console.WriteLine($" the name tool its {name} and the weight of its {weight} kg");
+
+    }
+    public override void Use()
+
+    {
+        Console.WriteLine("Hammer is used to make holes.");
+
+    }
+
+
+}
+
+
+
+
+
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -8,10 +8,56 @@ namespace ConsoleApp5
 {
     internal class Program
     {
-        List<string> tools = new List<string>();
         static void Main(string[] args)
-        {
+        { 
+
+            Drill drill = new Drill("drill",3.7);
+            drill.Describe();
+            drill.Use();
+            Console.WriteLine("");
+            Hammer hammer = new Hammer("hammer",1.7);
+            hammer.Describe();
+            hammer.Use();
+            Console.WriteLine("");
+            Wrench wrench = new Wrench("wrench", 1.1);
+            wrench.Describe();
+            wrench.Use();
+            Console.WriteLine("");
+            Saw saw = new Saw("saw", 0.9);
+            saw.Describe();
+            saw.Use();
+            Console.WriteLine("");
+            Screwdriver screwdriver = new Screwdriver("screwdriver", 0.15);
+            screwdriver.Describe();
+            screwdriver.Use();
+            Console.WriteLine("");
+            Pipecutter pipecutter = new Pipecutter("pipecutter", 1.3);
+            pipecutter.Describe();
+            pipecutter.Use();
+
+
+
+
+
+
+            List<Tool> tools = new List<Tool>();
+            tools.Add(drill);
+            tools.Add(hammer);
+            tools.Add(wrench);
+            tools.Add(saw);
+            tools.Add(pipecutter);
+
+
+
+
+
+
+
+
+
+
             Console.WriteLine("hello world");
+
         }
     }
 }
