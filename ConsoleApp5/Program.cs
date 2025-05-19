aa023e2032b4fbddfb6f6c452e67a499ad426271
Inon Guetta <inon16guetta@gmail.com>
2025-05-19 20:59:24 +0300

    finish version 02

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -14,23 +14,23 @@ namespace ConsoleApp5
             Drill drill = new Drill("drill",3.7);
             drill.Describe();
             drill.Use();
-            Console.WriteLine("");
+
             Hammer hammer = new Hammer("hammer",1.7);
             hammer.Describe();
             hammer.Use();
-            Console.WriteLine("");
+
             Wrench wrench = new Wrench("wrench", 1.1);
             wrench.Describe();
             wrench.Use();
-            Console.WriteLine("");
+
             Saw saw = new Saw("saw", 0.9);
             saw.Describe();
             saw.Use();
-            Console.WriteLine("");
+
             Screwdriver screwdriver = new Screwdriver("screwdriver", 0.15);
             screwdriver.Describe();
             screwdriver.Use();
-            Console.WriteLine("");
+
             Pipecutter pipecutter = new Pipecutter("pipecutter", 1.3);
             pipecutter.Describe();
             pipecutter.Use();
@@ -40,12 +40,10 @@ namespace ConsoleApp5
             Drill drill2 = new Drill("drill2",3.2);
             drill2.Describe();
             drill2.Use();
-            Console.WriteLine();
+
             Pipecutter pipecutter2 = new Pipecutter("pipecutter02", 1.1);
             pipecutter2.Describe();
             pipecutter2.Use();
-
-
 
             List<Tool> tools = new List<Tool>();
             tools.Add(drill);
